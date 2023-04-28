85c74d021343d1a419f977a9c8b62815b816bac9
DevSalma <devsalma6@gmail.com>
2023-04-28 12:39:33 +0100

    installed npm install jest as well did the class user and test for user

--- src/User.js
+++ src/User.js
@@ -1,5 +1,31 @@
 class User {
   // User code here
+  #password;
+  constructor(username, password, age, loggedIn){
+    this.username = username;
+    this.#password = password;
+    this.age = age;
+    this.loggedIn = false;
+  }
+  getOriginalPassword(){
+    return this.#password;
+  }
+  getPassword(){
+    const censoredPassword = this.#password;
+    return '*'.repeat(censoredPassword.length);
+  }
+  logIn(password){
+    let originalPassword = this.getOriginalPassword();
+    if(password === originalPassword){
+      return `${this.username} is logged in`
+    }else {
+      throw new Error('Incorrect password')
+    }
+  }
+  logOut(){
+    this.loggedIn = false;
+    return `${this.username} is logged out`;
+  }
 }
 
 module.exports = User
--- tests/User.test.js
+++ tests/User.test.js
@@ -1,13 +1,33 @@
 const User = require('../src/User')
 
 // User tests here
-
+describe('create new user', () => {
+    const user1 = new User('Mikes_account', 'thRee3', 20)
 // test username
-
+    test('has username', () => {
+        expect(user1.username).toBe('Mikes_account')
+    })
 // test password
-
+    test('has password', () => {
+        expect(user1.getOriginalPassword()).toBe('thRee3')
+    })
 // test age
-
+    test('has age', () => {
+        expect(user1.age).toBe(20)
+    })
+// test getpassword
+    test('returns an obscured password', () => {
+        expect(user1.getPassword()).toBe('******')
+    })
 // test login
-
+    test('logs in user', () => {
+        expect(user1.logIn('thRee3')).toBe('Mikes_account is logged in')
+        expect(() => {
+            user1.logIn('tHtee3')
+        }).toThrow('Incorrect password')
+    })
 // test logout
+    test('logs out user', () => {
+        expect(user1.logOut()).toBe('Mikes_account is logged out')
+    })
+})
